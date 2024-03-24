a71a650496fb79e897d5735f4c69f258fc6fda7f
romo2613 <javi.romero2613@gmail.com>
2024-03-24 22:41:30 +0100

    solucion problemas de z-index, fijacion de barra superior en vista movil

--- src/components/react/Marquee.tsx
+++ src/components/react/Marquee.tsx
@@ -2,8 +2,8 @@ import Marquee from "react-fast-marquee";
 
 export default function(){
   return (
-    <div>
-      <Marquee>
+    <div className="z-10">
+      <Marquee className="z-10">
          • PHP • Laravel • Laravel Livewire • Java • JavaScript • ReactJS • NextJS • JQuery • TailwindCSS • DaisyUI • ShadcnUI • BootStrap • CSS • HTML • MySQL • Ubuntu Server • AWS • CloudFlare • NGINX • aaPanel • DevilBox - Docker LAMP development stack • Postman • VScode
       </Marquee>
     </div>
