676678a2e4bc42fd108ee0bc9c4802e46eca51ac
romo2613 <36745834+romo2613@users.noreply.github.com>
2024-03-30 11:58:53 +0100

    añadidos temas persistentes y corregido el fallo que provocaba que al abrir el menu lateral en vista movil y volver a atras este se volviese a abrir en su estado anterior

---
+++ src/components/react/ThemeSelector.jsx
@@ -0,0 +1,25 @@
+
+export default function MyAwesomeThemeComponent() {
+
+    const handleThemeChange = (event) => {
+        localStorage.setItem("theme", event.target.value);
+    };
+
+
+    return (
+        <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn m-1">
+                <svg className="icon icon-tabler icons-tabler-outline icon-tabler-brush" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21v-4a4 4 0 1 1 4 4h-4" /><path d="M21 3a16 16 0 0 0 -12.8 10.2" /><path d="M21 3a16 16 0 0 1 -10.2 12.8" /><path d="M10.6 9a9 9 0 0 1 4.4 4.4" /></svg>
+                <svg className="hidden md:block icon icon-tabler icons-tabler-outline icon-tabler-chevron-down" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor" stroke-linecap="round"  stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 9l6 6l6 -6" /></svg>
+            </div>
+            <ul tabIndex={0} className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box">
+                <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="synthwave" value="synthwave" onChange={handleThemeChange} /></li>
+                <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Retro" value="retro" onChange={handleThemeChange} /></li>
+                <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Cyberpunk" value="cyberpunk" onChange={handleThemeChange} /></li>
+                <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Valentine" value="valentine" onChange={handleThemeChange} /></li>
+                <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Aqua" value="aqua" onChange={handleThemeChange} /></li>
+                <li><input type="radio" name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Night" value="night" onChange={handleThemeChange} /></li>
+            </ul>
+        </div>
+    );
+}
---
+++ src/layouts/preventOpenDrawer.js
@@ -0,0 +1,15 @@
+// En la version movil cierra el menu lateral cuando se hace click en algun elemento de navegacion del menu
+// para evitar que al volver atras el menu se quede abierto
+
+console.log("se ejecuta");
+
+var menuItems = document.querySelectorAll('.menu li a');
+
+for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', function () {
+        var drawerToggle = document.querySelector('#my-drawer');
+        drawerToggle.checked = false;
+    });
+}
+
+
