86eb28820c4c929bf3cce1f0845a4b1154689be0
romo2613 <36745834+romo2613@users.noreply.github.com>
2024-04-02 20:00:45 +0200

    algunos cambios estéticos

--- src/components/react/Marquee.tsx
+++ src/components/react/Marquee.tsx
@@ -2,14 +2,11 @@ import Marquee from "react-fast-marquee";
 import type { ComponentProps } from 'react';
 
 
-export default function(props: ComponentProps<typeof Marquee>){
+export default function MyComponent(props: ComponentProps<typeof Marquee>){
   return (
-    <div>
-      <Marquee {...props} className="z-10">
+      <Marquee {...props}>
          • PHP • Laravel • Laravel Livewire • Java • JavaScript • ReactJS • NextJS • JQuery • TailwindCSS • DaisyUI • ShadcnUI • BootStrap • CSS • HTML • MySQL • Ubuntu Server • AWS • CloudFlare • NGINX • aaPanel • DevilBox - Docker LAMP development stack • Postman • VScode
       </Marquee>
-    </div>
   )
 }
-
 
